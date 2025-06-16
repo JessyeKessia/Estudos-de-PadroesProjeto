ee57ba058be93a14c8436c4bde9a8fe319022de9
jessyekessia <jessyekessia16@gmail.com>
2025-06-16 00:08:09 -0300

    Finalizando o main

--- AbstractFactory/Exercicio2/Main.java
+++ AbstractFactory/Exercicio2/Main.java
@@ -2,13 +2,25 @@ package AbstractFactory.Exercicio2;
 
 public class Main {
     public static void main(String[] args) {
+        // Injeta a empresa que vai ter os produtos, o estilo será definido aqui
         EmpresaFactory appleFactory = new AppleFactory();
+
+        // cria os produtos, com bass nos métodos disponíveis na interface (retorna cada interface do produto dentro dos métodos)
         SmartPhone applePhone = appleFactory.createSmartPhone();
         Tablet appleTablet = appleFactory.createTablet();
         Earphone airpods = appleFactory.createEarphone();
 
+        System.out.println("Inicializa o celular Iphone");
+        // inicializa os o iphone com o sistema da apple
         applePhone.installSO();
         applePhone.initialize();
+
+        System.out.println("Inicializa o ipad");
+        // inicializa o tablet com o sistema da apple
+        appleTablet.installSO();
+        appleTablet.initialize();
+
+        // roda a autonomia de tempo do airpoids
         airpods.autonomia();
 
         EmpresaFactory samsungFactory = new SamsungFactory();
@@ -16,8 +28,17 @@ public class Main {
         Tablet samsungTablet = samsungFactory.createTablet();
         Earphone galaxyBuds = samsungFactory.createEarphone();
 
+        // inicializa o telefone com o sistema da samsung
+        System.out.println("Inicializa o celular da Apple");
         samsungPhone.installSO();
         samsungPhone.initialize();
-        airpods.autonomia();
+
+        // inicializa o tablet com o sistema da samsung
+        System.out.println("Inicializa o tablet da samsung");
+        samsungTablet.initialize();
+        samsungTablet.initialize();
+
+        // roda a autonomia de tempo do airpoids
+        galaxyBuds.autonomia();
     }
 }
--- AbstractFactory/Exercicio2/TabletApple.java
+++ AbstractFactory/Exercicio2/TabletApple.java
@@ -2,9 +2,9 @@ package AbstractFactory.Exercicio2;
 
 class TabletApple implements Tablet {
     public void installSO() {
-        System.out.println("Instalando Systema operacional do Android, da Samsung.");
+        System.out.println("Instalando Systema operacional da Apple, da IOS.");
     }
     public void initialize () {
-        System.out.println("Inicializando sistema operacional Android.");
+        System.out.println("Inicializando sistema operacional IOS.");
     }
 }
